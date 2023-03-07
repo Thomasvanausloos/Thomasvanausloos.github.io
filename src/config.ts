57145b5a014411e130083238fb6f90a1f63a3663
Thomas Vananausloos <thomas@Thomass-Laptop.local>
2023-03-07 08:15:17 +0100

    change some meta-data of the site

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,23 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+// example:
+// import { SITE_TITLE } from "../config";
+// ---
+
+// <PageLayout>
+  // <PageMeta title={`${SITE_TITLE} | Web Ninja`} slot="meta" />
+  // <Fragment slot="main">
+//     <Intro />
+//     <AboutTheTheme />
+//   </Fragment>
+// </PageLayout>
+
+export const SITE_TITLE = "Thomas Vanausloos";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
+  "Welcome to my personal website and blog";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+export const MY_NAME = "Thomas Vanausloos";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
